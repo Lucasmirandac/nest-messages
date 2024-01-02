d19e0da3349ef9fc1cad5ae7deef4897e611c2e5
Lucas Costa <lucas.costa@concert.com.br>
2024-01-02 20:07:17 -0300

    :wrench: fixing tabs

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,18 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
+	@Get()
+	listMessages() {}
 
-    @Get()
-    listMessages(){
+	@Get('/:id')
+	getMessage(@Param('id') id: string) {
+		console.log(id);
+	}
 
-    }
-
-    @Get('/:id')
-    getMessage(@Param('id') id: string){
-        console.log(id)
-    }
-
-    @Post()
-    createMessage(@Body() body: any){
-        console.log(body)
-    }
-
+	@Post()
+	createMessage(@Body() body: CreateMessageDto) {
+		console.log(body);
+	}
 }
