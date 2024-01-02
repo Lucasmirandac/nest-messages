d0be6b7099942f6e52d1d661ed5a361ef1f2309e
Lucas Costa <lucas.costa@concert.com.br>
2024-01-02 19:44:54 -0300

    :tada: init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { MessagesModule } from './messages/messages.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(
+    new ValidationPipe()
+  )
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string){
+        console.log(id)
+    }
+
+    @Post()
+    createMessage(@Body() body: any){
+        console.log(body)
+    }
+
+}
